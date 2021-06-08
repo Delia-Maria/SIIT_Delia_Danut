d2262fa9778aceaa85bde9f22c8b3a5d27dc21a0
Delia-Maria <delia_humeniuc@yahoo.com>
2021-06-09 00:42:07 +0300

    add

--- 07_week_homework/app.js
+++ 07_week_homework/app.js
@@ -1,27 +1,13 @@
-// Un timer 00:00:00 care isi face update in real time
-
-// task1: Sa afisam in browser 00:00:00
-// Sa avem in cod posibilitatea de a le face update pe rand
-
 var hourElement = document.getElementById("hour");
 var minuteElement = document.getElementById("minutes");
 var secondElement = document.getElementById("seconds");
 
-// task2: Sa facem update doar la secunde
-// Task3: Sa afisam secundele
-// Task4: Sa facem update la minute + reset la secunde
-// Nu ne garanteaza rularea la fiecare sec
-// for(var index = 0; index<60; index++){
-//   console.log(index)
-// }
+
 var minutesCount = 0;
-var secondsCount = 55;
+var secondsCount = 00;
 var hoursCount = 0;
 
-function updateSeconds() {
-    secondsCount = secondsCount + 1;
-
-    // secondElement.innerText = secondsCount < 10 ? "0" + secondsCount: secondsCount
+function updateMinutes() {
     if (secondsCount === 60) {
         secondsCount = 0;
         minutesCount++;
@@ -30,12 +16,10 @@ function updateSeconds() {
             minutesCount < 10 ? "0" + minutesCount : minutesCount;
     }
 
-    if (secondsCount < 10) {
-        secondElement.innerText = "0" + secondsCount;
-    } else {
-        secondElement.innerText = secondsCount;
-    }
 
+}
+
+function updateHours() {
     if (minutesCount === 60) {
         minutesCount = 0;
         hoursCount++;
@@ -44,6 +28,64 @@ function updateSeconds() {
             hoursCount < 10 ? "0" + hoursCount : hoursCount;
     }
 
-
 }
-setInterval(updateSeconds, 1000);
+
+function updateDomTimer() {
+
+    if (secondsCount < 10) {
+        secondElement.innerText = "0" + secondsCount;
+    } else {
+        secondElement.innerText = secondsCount;
+    }
+}
+
+function updateTime() {
+    secondsCount = secondsCount + 1;
+
+    updateHours();
+    updateMinutes();
+    updateDomTimer();
+}
+//Adaugam functionalitate la butonul de start
+let btnCount = 0;
+var intervalId;
+const btnStart = document.getElementById('start-btn');
+btnStart.addEventListener('click', function() {
+    if (btnCount < 1) {
+
+        intervalId = setInterval(updateTime, 1000);
+    }
+    btnCount++;
+})
+
+//Adaugam functionalitate la butonul de stop
+
+const btnStop = document.getElementById('stop-btn');
+btnStop.addEventListener('click', function() {
+    clearInterval(intervalId);
+    btnCount = 0;
+
+})
+
+//Adaugam functionalitate la butonul de reset
+
+const btnReset = document.getElementById('reset-btn');
+btnReset.addEventListener('click', function() {
+        secondsCount = 0;
+        document.getElementById("hour").innerHTML = '00';
+        document.getElementById("minutes").innerHTML = '00';
+        document.getElementById("seconds").innerHTML = '00';
+
+
+    })
+    //Adaugam functionalitate la butonul de save
+
+const btnSave = document.getElementById('save-btn');
+btnSave.addEventListener('click', function() {
+    const container = document.querySelector('container');
+
+
+
+    container.innerHTML = 'span';
+
+})
